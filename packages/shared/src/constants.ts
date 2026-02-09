4282d67e24ff0748d27dd933306e480a4bf11b05
Ashutosh Jha <ashutoshj887@gmail.com>
2026-02-09 13:32:33 +0530

    chore: update forensics queue and dead letter queue naming convention

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,8 +1,8 @@
 export const INGEST_MAX_BODY_BYTES = 1 * 1024 * 1024;
 export const GEMINI_MAX_OUTPUT_TOKENS = 4096;
 
-export const FORENSICS_QUEUE_NAME = "faultline:forensics";
-export const FORENSICS_DLQ_NAME = "faultline:forensics:failed";
+export const FORENSICS_QUEUE_NAME = "faultline-forensics";
+export const FORENSICS_DLQ_NAME = "faultline-forensics-failed";
 
 export const INGEST_RATE_LIMIT_WINDOW_MS = 60_000;
 export const INGEST_RATE_LIMIT_MAX_REQUESTS = 100;
